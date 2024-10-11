b5d47f5a7ca024da0a52c1d13968747cd64bf6b1
otaviootavio <otaviovacari@usp.br>
2024-10-11 15:29:03 -0300

    Add ultrasonic

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -9,6 +9,59 @@
 #include "lwip/err.h" //light weight ip packets error handling
 #include "lwip/sys.h" //system applications for light weight ip apps
 
+
+
+#include <stdbool.h>
+#include <freertos/task.h>
+#include <ultrasonic.h>
+#include <esp_err.h>
+
+
+#define MAX_DISTANCE_CM 500 // 5m max
+
+#define TRIGGER_GPIO 22
+#define ECHO_GPIO 23
+
+
+void ultrasonic_test(void *pvParameters)
+{
+    ultrasonic_sensor_t sensor = {
+        .trigger_pin = TRIGGER_GPIO,
+        .echo_pin = ECHO_GPIO
+    };
+
+    ultrasonic_init(&sensor);
+
+    while (true)
+    {
+        float distance;
+        esp_err_t res = ultrasonic_measure(&sensor, MAX_DISTANCE_CM, &distance);
+        if (res != ESP_OK)
+        {
+            printf("Error %d: ", res);
+            switch (res)
+            {
+                case ESP_ERR_ULTRASONIC_PING:
+                    printf("Cannot ping (device is in invalid state)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_PING_TIMEOUT:
+                    printf("Ping timeout (no device found)\n");
+                    break;
+                case ESP_ERR_ULTRASONIC_ECHO_TIMEOUT:
+                    printf("Echo timeout (i.e. distance too big)\n");
+                    break;
+                default:
+                    printf("%s\n", esp_err_to_name(res));
+            }
+        }
+        else
+            printf("Distance: %0.04f cm\n", distance*100);
+            // Send to database async
+
+        vTaskDelay(pdMS_TO_TICKS(1000));
+    }
+}
+
 const char *ssid = "Oirs";
 const char *pass = "vivaembarcado";
 int retry_num=0;
@@ -68,5 +121,5 @@ void app_main(void)
 {
 nvs_flash_init();
 wifi_connection();
-    
+xTaskCreate(ultrasonic_test, "ultrasonic_test", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }
